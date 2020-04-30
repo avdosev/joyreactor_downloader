0418e15bbfd0c157b2a5e7ffac97bb8f5d654786
tankistqazwsx <alexei.bezruchenko@yandex.ru>
2020-04-30 22:46:59 +0400

    исправил косяк, из-за которого парсилась только первая страница

--- src/script.py
+++ src/script.py
@@ -9,7 +9,7 @@ from reactor_parse import scrapPage, getPrevPages, parse_html
 from safe_get import fetch_html
 
 # download - качает указанные картинки
-# saveSrc - сохраняет ссылки на указанные картинки
+# save_source - сохраняет ссылки на указанные картинки
 # bulk - при прерывании по Ctrl+C сохраняет наработки и при следующем вызове продолжает работу
 
 async def bulk_save_source_image_by_tag(url):
@@ -18,17 +18,17 @@ async def bulk_save_source_image_by_tag(url):
     last_page_checked = url
     name = getName(url)
 
-    filename = os.path.join("temp", f"{name}_bulkSaveSrc_imageFromTag.txt")
-
     if name in {"all", "best", "new"}:
         print(str.format("убираем {0}...", name))
         name = getName(cutLastFrom(url, '/'))
         print(str.format("получаем {0}", name))
 
+    progress_file_name = os.path.join("temp", f"{name}_bulkSaveSrc_imageFromTag.txt")
+
     output_name = f"url_{name}.txt"
-    if os.path.isfile(filename):  # если файла нет, то и прогресс не надо считывать
+    if os.path.isfile(progress_file_name):  # если файла нет, то и прогресс не надо считывать
         print("найден файл прогресса")
-        with open(filename, "r") as f:
+        with open(progress_file_name, "r") as f:
             last_page_checked = f.read()
     else:
         print("файл прогресса не найден")
@@ -44,23 +44,18 @@ async def bulk_save_source_image_by_tag(url):
         html = await download_parsed_page(last_page_checked, session)
         tag_pages_list = getPrevPages(url, html)
         try:
-            async def download_and_parse(page, session, queue):
-                html = await download_parsed_page(page, session)
-                result = scrapPage(html)
-                src_list = result['links']
-                censored_list = result['censored']
-                await queue.put((src_list, censored_list, page))
-
             async def load_parsed_data_to_file(queue):
                 nonlocal last_page_checked
                 while True:
                     src_list, censored_list, page = await queue.get()
+                    print(str.format("Начата обработка страницы {0}", page))
                     last_page_checked = page
                     # следующий кусок кода независимый друг от друга и мы можем выполнить их паралельно
                     await asyncio.gather(*[
                         append_to_file(src_list, output_name),
-                        append_to_censored(censored_list)
+                        append_to_file(censored_list, "logs/censored.txt")
                     ])
+                    print(str.format("Страница {0} обработана", page))
                     queue.task_done()
 
             queue = asyncio.Queue()
@@ -71,12 +66,16 @@ async def bulk_save_source_image_by_tag(url):
             file_loader.cancel()
             await asyncio.gather(file_loader, return_exceptions=True)
         except KeyboardInterrupt:
-            with open(filename, "w") as f:
+            with open(progress_file_name, "w") as f:
                 print(f"Последняя страница ", last_page_checked, " сохранена")
                 f.write(last_page_checked)
 
-async def append_to_censored(censored_links):
-    await append_to_file(censored_links, "logs/censored.txt")
+async def download_and_parse(page, session, queue):
+    html = await download_parsed_page(page, session)
+    result = scrapPage(html)
+    src_list = result['links']
+    censored_list = result['censored']
+    await queue.put((src_list, censored_list, page))
 
 # Запрашивает страницу по адресу
 async def download_parsed_page(url, session):
